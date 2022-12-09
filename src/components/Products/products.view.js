7620a6c693522489d7281786ccce6dce0b3e5c70
Luka Tarman <luka.tarman@gmail.com>
2022-12-09 15:39:22 +0100

    Added products component

--- src/components/Products/products.view.js
+++ src/components/Products/products.view.js
@@ -1,13 +1,32 @@
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Image } from "react-bootstrap";
+import image from "../../assets/img/shoes.jpg";
 
-const Products = () => {
+const Products = ({ data = [1, 2, 3, 4, 5, 6, 7, 8] }) => {
+  const productItems = data.map((item) => {
+    return (
+      <Col>
+        <div className="m-5">
+          <Image
+            img
+            src={image}
+            height={180}
+            width={180}
+            className="d-flex flex-column align-items-center mx-auto"
+          />
+          <div className="d-flex flex-column align-items-center">
+            Tommy Hilfiger Shoes
+          </div>
+          <div className="d-flex flex-column align-items-center">20,99 EUR</div>
+        </div>
+      </Col>
+    );
+  });
+
   return (
     <Container>
-      <Row>
-        <Col sm={true}>Test Product</Col>
-        <Col sm={true}>Test Product</Col>
-        <Col sm={true}>Test Product</Col>
-        <Col sm={true}>Test Product</Col>
+      <h2 className="my-5">Available Products</h2>
+      <Row lg={4} md={2} sm={2} xs={1}>
+        {productItems}
       </Row>
     </Container>
   );
