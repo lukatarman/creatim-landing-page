cbe37d692ecacff8558054bbbf8a70549c8bde06
Luka Tarman <luka.tarman@gmail.com>
2022-12-09 12:43:11 +0100

    added app component

---
+++ src/pages/App/app.view.js
@@ -0,0 +1,14 @@
+import NavbarCustom from "../../components/NavbarCustom/navbar.custom.view.js";
+import CarouselCustom from "../../components/CarouselCustom/carousel.custom.view.js";
+
+const App = () => {
+  return (
+    <div>
+      <NavbarCustom />
+      <CarouselCustom />
+      <div style={{ marginBottom: "2000px" }}></div>
+    </div>
+  );
+};
+
+export default App;
