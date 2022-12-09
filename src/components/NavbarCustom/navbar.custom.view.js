81fcc5a6541245df78274a071507221ff1af6606
Luka Tarman <luka.tarman@gmail.com>
2022-12-09 12:42:28 +0100

    added navbar with mega menu

---
+++ src/components/NavbarCustom/navbar.custom.view.js
@@ -0,0 +1,56 @@
+import { Container, Nav, Navbar, NavDropdown, Col, Row } from "react-bootstrap";
+
+function NavbarCustom() {
+  return (
+    <Navbar fixed="top" bg="light" expand="lg">
+      <Container>
+        <Navbar.Brand href="#home">Best Clothing Shop</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <NavDropdown
+              title="Mega-Menu"
+              id="basic-nav-dropdown"
+              className="custom-dropdown"
+            >
+              <Container>
+                <Row>
+                  <Col className="mb-3">
+                    <label>Men:</label>
+                    <NavDropdown.Item>Shirts</NavDropdown.Item>
+                    <NavDropdown.Item>Pants</NavDropdown.Item>
+                    <NavDropdown.Item>Shoes</NavDropdown.Item>
+                    <NavDropdown.Item>Accessories</NavDropdown.Item>
+                  </Col>
+                  <Col className="mb-3">
+                    <label>Women:</label>
+                    <NavDropdown.Item>Shirts</NavDropdown.Item>
+                    <NavDropdown.Item>Pants</NavDropdown.Item>
+                    <NavDropdown.Item>Shoes</NavDropdown.Item>
+                    <NavDropdown.Item>Accessories</NavDropdown.Item>
+                  </Col>
+                  <Col className="mb-3">
+                    <label>Kids:</label>
+                    <NavDropdown.Item>Shirts</NavDropdown.Item>
+                    <NavDropdown.Item>Pants</NavDropdown.Item>
+                    <NavDropdown.Item>Shoes</NavDropdown.Item>
+                    <NavDropdown.Item>Accessories</NavDropdown.Item>
+                  </Col>
+                  <Col className="mb-3">
+                    <NavDropdown.Item>
+                      <strong>Your Account</strong>
+                    </NavDropdown.Item>
+                    <NavDropdown.Item>Orders</NavDropdown.Item>
+                    <NavDropdown.Item>Browsing History</NavDropdown.Item>
+                  </Col>
+                </Row>
+              </Container>
+            </NavDropdown>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
+export default NavbarCustom;
