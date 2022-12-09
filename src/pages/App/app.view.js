4e1d534e78a0e3262fdcc9dc9cd22f83a14080ae
Luka Tarman <luka.tarman@gmail.com>
2022-12-09 13:39:19 +0100

    added padding for navbar

--- src/pages/App/app.view.js
+++ src/pages/App/app.view.js
@@ -3,7 +3,7 @@ import BannerSlider from "../../components/BannerSlider/banner.slider.view.js";
 
 const App = () => {
   return (
-    <div>
+    <div className="pt-5">
       <NavbarCustom />
       <BannerSlider options={carouselOptions} />
       
