a6d8001dc8b2493f3beb453416b847db84d9b609
Luka Tarman <luka.tarman@gmail.com>
2022-12-09 14:32:25 +0100

    added products component

---
+++ src/components/Products/products.view.js
@@ -0,0 +1,16 @@
+import { Container, Row, Col } from "react-bootstrap";
+
+const Products = () => {
+  return (
+    <Container>
+      <Row>
+        <Col sm={true}>Test Product</Col>
+        <Col sm={true}>Test Product</Col>
+        <Col sm={true}>Test Product</Col>
+        <Col sm={true}>Test Product</Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default Products;
