ce1400dc43c30c3c4abad0e146acb0043ffec2bf
Luka Tarman <luka.tarman@gmail.com>
2022-12-09 13:39:26 +0100

    added carouselOptions

--- src/pages/App/app.view.js
+++ src/pages/App/app.view.js
@@ -2,6 +2,12 @@ import NavbarCustom from "../../components/NavbarCustom/navbar.custom.view.js";
 import BannerSlider from "../../components/BannerSlider/banner.slider.view.js";
 
 const App = () => {
+  const carouselOptions = [
+    { label: "First Slide", text: "50% off until Friday" },
+    { label: "Second Slide", text: "Even bigger discounts!" },
+    { label: "Third Slide", text: "90% off for our Black Friday sale!" },
+  ];
+
   return (
     <div className="pt-5">
       <NavbarCustom />
