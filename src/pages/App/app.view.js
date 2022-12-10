12ebc48bb607b8e48984809dd8d02dc5bdbeacdc
Luka Tarman <luka.tarman@gmail.com>
2022-12-10 10:09:10 +0100

    fetched data from mock db
    # used productList useState
    # passed in state to child

--- src/pages/App/app.view.js
+++ src/pages/App/app.view.js
@@ -1,19 +1,34 @@
+import { useEffect, useState } from "react";
 import NavbarCustom from "../../components/NavbarCustom/navbar.custom.view.js";
 import BannerSlider from "../../components/BannerSlider/banner.slider.view.js";
 import Products from "../../components/Products/products.view.js";
+import { getItems } from "../../adapter/mock.http.client.js";
 
 const App = () => {
+  const [productList, setProductList] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await getItems();
+      setProductList(response);
+    };
+
+    fetchData();
+  }, []);
+
   const carouselOptions = [
     { label: "First Slide", text: "50% off until Friday" },
     { label: "Second Slide", text: "Even bigger discounts!" },
     { label: "Third Slide", text: "90% off for our Black Friday sale!" },
   ];
 
+  getItems();
+
   return (
     <div className="pt-5">
       <NavbarCustom />
       <BannerSlider options={carouselOptions} />
-      <Products />
+      <Products products={productList} />
     </div>
   );
 };
