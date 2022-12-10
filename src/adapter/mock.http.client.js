5fbdbde171ce5513f804d1955df8e0a6551ffa06
Luka Tarman <luka.tarman@gmail.com>
2022-12-10 10:08:18 +0100

    added mock getItems function

--- src/adapter/mock.http.client.js
+++ src/adapter/mock.http.client.js
@@ -1,1 +1,9 @@
-export const getItems = async () => {};
+import { storeItems } from "../assets/mockDatabaseResponse/mock.database.response.js";
+
+export const getItems = async () => {
+  return await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(storeItems);
+    }, 150);
+  });
+};
