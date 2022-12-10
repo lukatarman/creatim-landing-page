5d830cd66e3030f4ac2988c44ec5c3fde695c646
Luka Tarman <luka.tarman@gmail.com>
2022-12-10 15:46:17 +0100

    added another mock http client request

--- src/adapter/mock.http.client.js
+++ src/adapter/mock.http.client.js
@@ -1,9 +1,20 @@
-import { storeItems } from "../assets/mockDatabaseResponse/mock.database.response.js";
+import {
+  storeItems,
+  fourNewStoreItems,
+} from "../assets/mockDatabaseResponse/mock.database.response.js";
 
 export const getItems = async () => {
   return await new Promise((resolve) => {
     setTimeout(() => {
       resolve(storeItems);
+    }, 150);
+  });
+};
+
+export const getNewItems = async () => {
+  return await new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(fourNewStoreItems);
     }, 150);
   });
 };
