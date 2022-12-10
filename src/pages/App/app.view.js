d04e57d899d39c1f83a19e6b2f4204a1b739a27c
Luka Tarman <luka.tarman@gmail.com>
2022-12-10 15:47:20 +0100

    added additional state and useeffect

--- src/pages/App/app.view.js
+++ src/pages/App/app.view.js
@@ -2,10 +2,11 @@ import { useEffect, useState } from "react";
 import NavbarCustom from "../../components/NavbarCustom/navbar.custom.view.js";
 import BannerSlider from "../../components/BannerSlider/banner.slider.view.js";
 import Products from "../../components/Products/products.view.js";
-import { getItems } from "../../adapter/mock.http.client.js";
+import { getItems, getNewItems } from "../../adapter/mock.http.client.js";
 
 const App = () => {
   const [productList, setProductList] = useState([]);
+  const [isFetchingMoreData, setIsFetchingMoreData] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -16,19 +17,29 @@ const App = () => {
     fetchData();
   }, []);
 
+  useEffect(() => {
+    const fetchData = async () => {
+      console.log(productList);
+      const response = await getNewItems();
+      console.log(response);
+      setProductList([...productList, ...response]);
+    };
+
+    if (isFetchingMoreData) fetchData();
+    setIsFetchingMoreData(false);
+  }, [isFetchingMoreData]);
+
   const carouselOptions = [
     { label: "First Slide", text: "50% off until Friday" },
     { label: "Second Slide", text: "Even bigger discounts!" },
     { label: "Third Slide", text: "90% off for our Black Friday sale!" },
   ];
 
-  getItems();
-
   return (
     <div className="pt-5">
       <NavbarCustom />
       <BannerSlider options={carouselOptions} />
-      <Products products={productList} />
+      <Products products={productList} setFetchingData={setIsFetchingMoreData} />
     </div>
   );
 };
