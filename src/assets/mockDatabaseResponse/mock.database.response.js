aafef9a40b46ab96ec3b337705b742c2b580806b
Luka Tarman <luka.tarman@gmail.com>
2022-12-10 15:46:23 +0100

    added mock db response

--- src/assets/mockDatabaseResponse/mock.database.response.js
+++ src/assets/mockDatabaseResponse/mock.database.response.js
@@ -4,41 +4,76 @@ export const storeItems = [
   {
     name: "Tommy Hilfiger Shoes",
     price: "20,99 EUR",
+    sex: "Male",
     image,
   },
   {
     name: "Tommy Hilfiger Shoes",
     price: "20,99 EUR",
+    sex: "Male",
     image,
   },
   {
     name: "Tommy Hilfiger Shoes",
     price: "20,99 EUR",
+    sex: "Male",
     image,
   },
   {
     name: "Tommy Hilfiger Shoes",
     price: "20,99 EUR",
+    sex: "Male",
     image,
   },
   {
     name: "Tommy Hilfiger Shoes",
     price: "20,99 EUR",
+    sex: "Male",
     image,
   },
   {
     name: "Tommy Hilfiger Shoes",
     price: "20,99 EUR",
+    sex: "Male",
     image,
   },
   {
     name: "Tommy Hilfiger Shoes",
     price: "20,99 EUR",
+    sex: "Male",
     image,
   },
   {
     name: "Tommy Hilfiger Shoes",
     price: "20,99 EUR",
+    sex: "Male",
+    image,
+  },
+];
+
+export const fourNewStoreItems = [
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    sex: "Male",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    sex: "Male",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    sex: "Male",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    sex: "Male",
     image,
   },
 ];
