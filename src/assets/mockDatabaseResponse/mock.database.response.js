2fb85dadbbf7c1f237c248ff03894b928fd1fc66
Luka Tarman <luka.tarman@gmail.com>
2022-12-10 10:08:28 +0100

    added mock db response data

---
+++ src/assets/mockDatabaseResponse/mock.database.response.js
@@ -0,0 +1,44 @@
+import image from "../img/shoes.jpg";
+
+export const storeItems = [
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    image,
+  },
+];
