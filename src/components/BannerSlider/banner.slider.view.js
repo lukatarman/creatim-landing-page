5113becfd5bea267be76de213e327348de2ab4b9
Luka Tarman <luka.tarman@gmail.com>
2022-12-10 10:08:36 +0100

    added key prop to children

--- src/components/BannerSlider/banner.slider.view.js
+++ src/components/BannerSlider/banner.slider.view.js
@@ -1,9 +1,9 @@
 import Carousel from "react-bootstrap/Carousel";
 
 function BannerSlider({ options }) {
-  const carouselItems = options.map((item) => {
+  const carouselItems = options.map((item, index) => {
     return (
-      <Carousel.Item>
+      <Carousel.Item key={index}>
         <div className="carousel-item"></div>
         <Carousel.Caption>
           <h3>{item.label}</h3>
