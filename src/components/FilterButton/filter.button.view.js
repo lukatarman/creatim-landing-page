aa017ce2dc7ef0446f7563360b72bad5d02d65e8
Luka Tarman <luka.tarman@gmail.com>
2022-12-12 10:20:40 +0100

    added margin to filterbutton

--- src/components/FilterButton/filter.button.view.js
+++ src/components/FilterButton/filter.button.view.js
@@ -1,6 +1,6 @@
 const FilterButton = ({ name, handleClick }) => {
   return (
-    <button className="button-filter" onClick={handleClick}>
+    <button className="button-filter m-1" onClick={handleClick}>
       {name}
     </button>
   );
