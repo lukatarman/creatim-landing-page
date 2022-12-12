c981887563e36afb596a9029e5abbdf6fe253244
Luka Tarman <luka.tarman@gmail.com>
2022-12-12 14:03:51 +0100

    adjusted db response MOCK

--- src/assets/mockDatabaseResponse/mock.database.response.js
+++ src/assets/mockDatabaseResponse/mock.database.response.js
@@ -1,90 +1,338 @@
 import image from "../img/shoes.jpg";
 
-export const storeItems = [
-  {
-    name: "Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-  {
-    name: "Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-  {
-    name: "Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-  {
-    name: "Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-  {
-    name: "Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-  {
-    name: "Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-  {
-    name: "Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-  {
-    name: "Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-];
+export const getStoreItems = () => {
+  return storeItems.slice(0, 8);
+};
 
-export const fourNewStoreItems = [
-  {
-    name: "NEW Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-  {
-    name: "NEW Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-  {
-    name: "NEW Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-  {
-    name: "NEW Tommy Hilfiger Shoes",
-    price: "20,99 EUR",
-    image,
-  },
-];
+export const getFilteredStoreItems = (filters) => {
+  filteredStoreItems = storeItems.filter((item) => filters.includes(item.age));
+  return filteredStoreItems;
+};
 
-export const filteredItems = [
+export const getAdditionalStoreItems = (filters, existingItems, additionalAmount) => {
+  if (filters.length > 0) {
+    return filteredStoreItems.slice(
+      existingItems.length,
+      existingItems.length + additionalAmount
+    );
+  }
+
+  return storeItems.slice(existingItems.length, existingItems.length + additionalAmount);
+};
+
+let filteredStoreItems;
+
+const storeItems = [
   {
-    name: "Kid's Tommy Hilfiger Shoes",
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Kid's Shoes",
+    price: "20,99 EUR",
+    age: "kids",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Kid's Shoes",
+    price: "20,99 EUR",
+    age: "kids",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Kid's Shoes",
     price: "12,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Tommy Hilfiger Kid's Shoes",
+    price: "12,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Kid's Shoes",
+    price: "20,99 EUR",
+    age: "kids",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
     image,
   },
   {
     name: "Kid's Tommy Hilfiger Shoes",
     price: "12,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Kid's Shoes",
+    price: "20,99 EUR",
+    age: "kids",
     image,
   },
   {
     name: "Kid's Tommy Hilfiger Shoes",
     price: "12,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
     image,
   },
   {
     name: "Kid's Tommy Hilfiger Shoes",
     price: "12,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Kid's Tommy Hilfiger Shoes",
+    price: "12,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Kid's Tommy Hilfiger Shoes",
+    price: "12,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Kid's Tommy Hilfiger Shoes",
+    price: "12,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Kid's Tommy Hilfiger Shoes",
+    price: "12,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Kid's Tommy Hilfiger Shoes",
+    price: "12,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "NEW Tommy Hilfiger Shoes",
+    price: "20,99 EUR",
+    age: "adult",
+    image,
+  },
+  {
+    name: "Kid's Tommy Hilfiger Shoes",
+    price: "12,99 EUR",
+    age: "adult",
     image,
   },
 ];
